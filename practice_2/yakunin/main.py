89878838802f5d30b2630b28866eb5ca21e37d2d
Alex48Yakunin <sanchezz.jakunin@rambler.ru>
2023-11-11 16:24:51 +0300

    adds a second practice

--- practice_2/yakunin/main.py
+++ practice_2/yakunin/main.py
@@ -1,2 +1,65 @@
-# Напишите ваш код тут
+# !pip install transformers torch PIL streamlit
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import io
+import torch
+from PIL import Image
+import streamlit as st
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    model.to(device)
+    return model
+
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+def predict_step(image_paths):
+  images = []
+  for image_path in image_paths:
+    i_image = image_path.convert('RGB')
+
+    images.append(i_image)
+
+  pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+  pixel_values = pixel_values.to(device)
+
+  output_ids = model.generate(pixel_values, **gen_kwargs)
+
+  preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+  preds = [pred.strip() for pred in preds]
+  return preds
+
+def load_image():
+    """Создание формы для загрузки изображения"""
+    # Форма для загрузки изображения средствами Streamlit
+    uploaded_file = st.file_uploader(
+        label='Выберите изображение для преобразования')
+    if uploaded_file is not None:
+        # Получение загруженного изображения
+        image_data = uploaded_file.getvalue()
+        # Показ загруженного изображения на Web-странице средствами Streamlit
+        st.image(image_data)
+        # Возврат изображения в формате PIL
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+# Загружаем предварительно обученную модель
+model = load_model()
+
+# Выводим заголовок страницы средствами Streamlit     
+st.title('Преобразователь изображения в текст')
+# Вызываем функцию создания формы загрузки изображения
+img = load_image()
+
+result = st.button('Преобразовать изображение')
+if result:
+    st.write(predict_step([img]))
